a4d733a5099e93974c90531015acaa12120bd397
Sippakorn-th <sippakorn.th@gmail.com>
2025-08-07 15:37:24 +0700

    update useCounter.spec.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
-    environment: 'jsdom'
-  }
+    environment: 'jsdom',
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 });
